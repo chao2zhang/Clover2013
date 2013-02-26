535012ce922963293a968a2e2682485ec957d418
Chao Zhang <zhangchao6865@gmail.com>
2013-02-27 00:40:12 +0800

    add trigger for weibo

--- actions/oauth/weibo.py
+++ actions/oauth/weibo.py
@@ -40,17 +40,18 @@ except ImportError:
         from django.utils import simplejson
         parse_json = lambda s: simplejson.loads(s)
 
-class WeiboClient(object):
-    def __init__(self, api_key = WeiboConfig.APP_API_KEY, secret_key = WeiboConfig.APP_SECRET_KEY):
+class Weibo(object):
+    def __init__(self, api_key = WeiboConfig.APP_API_KEY, secret_key = WeiboConfig.APP_SECRET_KEY, access_token = '805ebefa6fcce01846927e945c49c922'):
         self.api_key = api_key
         self.secret_key = secret_key
+        self.access_token = access_token
     
     def auth(self):
         args = {
             'client_id': self.api_key,
             'response_type': 'code',
             'redirect_uri': WeiboConfig.LOGIN_SUCCESS,
-            #'scope': 'status_update',
+            'scope': 'status_update',
         }
         url = "%s?%s" % (WeiboConfig.AUTHORIZATION_URI, urllib.urlencode(args))
         print 'Please authorize: ' + url
@@ -65,17 +66,47 @@ class WeiboClient(object):
         response = urllib2.urlopen(WeiboConfig.ACCESS_TOKEN_URI, urllib.urlencode(args)).read()
         print response
         self.access_token = parse_json(response)["access_token"]
+        return self.access_token
 
-    def request(self, uri, params = {}):
+    def post(self, uri, params = {}):
         """
         Fetches the given method's response returning from Weibo API.
         Send a POST request to the given method with the given params.
         """
         params["access_token"] = self.access_token
-        post_data = None if not params else urllib.urlencode(params)
-        response = urllib2.urlopen(uri, post_data).read()
+        params = urllib.urlencode(params)
+        response = urllib2.urlopen(uri, params).read()
         j = parse_json(response)
         return j
 
-    def update_status(self, status):
-        return self.request('https://api.weibo.com/2/statuses/update.json', {'status': status})
+    def get(self, uri, params = {}):
+        params["access_token"] = self.access_token
+        params = urllib.urlencode(params)
+        response = urllib2.urlopen('%s?%s' % (uri, params)).read() 
+        j = parse_json(response)
+        return j
+
+    def update_status(self, **kwargs):
+        return self.post('https://api.weibo.com/2/statuses/update.json', kwargs)
+
+    def friend_timeline(self, **kwargs):
+        if kwargs:
+            return self.get('https://api.weibo.com/2/statuses/friends_timeline.json', kwargs)
+        else:
+            return self.get('https://api.weibo.com/2/statuses/friends_timeline.json')
+
+    def trigger(self, created_at=None, user=None, tag=None):
+        l = self.friend_timeline().get('statuses')
+        if not l: return 0
+        i = 0
+        while created_at and l[:-1]['created_at'] > created_at:
+            i += 1
+            ll = self.friend_timeline({'page': i}).get('statuses')
+            if ll: break
+            l.append(ll)
+        if user:
+            l = filter(lambda x: x['user']['name'] == user or x['user']['screen_name'] == user, l)
+        if tag:
+            l = filter(lambda x: x['text'].find(tag) > -1, l)
+        return len(l)
+
