573ffbb6051bd40c607cae61e30a150914e845d0
cxhiano <cxhiano@gmail.com>
2013-03-09 14:58:19 +0800

    test renren mail weather stock

--- backend/actions/PostRenren.py
+++ backend/actions/PostRenren.py
@@ -2,16 +2,18 @@ from helper import *
 from lib.renren import RenrenClient
 
 def post(pending_info, action_info):
-	print pending_info
-	print '-' * 50
-	print action_info
-	print '-' * 50
 	try:
 		user_info, acc_info = getInfo(pending_info['action_id'], 'app_renrenaccount')
 
 		renren = RenrenClient(acc_info['access_token'])
-		renren.setStatus(pending_info['content'])
+		renren.setStatus(pending_info['content'].encode('utf-8'))
 
 	except:
+		print 'post renren failed'
+		print '-' * 50
+		print pending_info
+		print '-' * 50
+		print action_info
+		print '-' * 50
 		return False
 	return True
